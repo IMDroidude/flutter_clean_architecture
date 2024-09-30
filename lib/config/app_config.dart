cfd077ddfaf39440a95a3383fbcd02aa8e2a48fc
ZarEAhmer <zare.ahmer@appinsnap.com>
2024-09-30 16:45:05 +0500

    update directories

---
+++ lib/config/app_config.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/widgets.dart';
+
+class AppConfig {
+
+  // app init
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_architecture/myapp.dart';
+import 'package:flutter_clean_architecture/config/app_config.dart';
+import 'package:flutter_clean_architecture/app/myapp.dart';
 
 //push coce at https://github.com/IMDroidude/flutter_clean_architecture.git
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  await AppConfig.init();
   runApp(const MyApp());
 }
