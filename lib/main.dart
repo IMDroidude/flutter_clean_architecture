70016fb9a047495664b984906c0be49a741dc129
ZarEAhmer <zare.ahmer@appinsnap.com>
2024-11-28 17:18:46 +0500

    minor tweask

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_clean_architecture/config/app_config.dart';
 import 'package:flutter_clean_architecture/app/myapp.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 //push coce at https://github.com/IMDroidude/flutter_clean_architecture.git
 // void main() {
@@ -10,10 +12,48 @@ import 'package:flutter_clean_architecture/app/myapp.dart';
 
 /// https://github.com/himanshu077/news-flutter/blob/7c3f32f38512091e55bfd3c66e093aec810e7c81/lib/main.dart
 
-void main() async {
-  await AppConfig.init();
-  runApp(const MyApp());
+// void main() async {
+//   await AppConfig.init();
+//   runApp(const MyApp());
 
+//   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+//     // systemNavigationBarColor: COLORS.grey,
+//     // statusBarColor: COLORS.grey,
+//     systemNavigationBarColor: Colors.transparent,
+//     statusBarColor: Colors.transparent,
+//     statusBarIconBrightness: Brightness.dark,
+//   ));
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  /// https://github.com/wednesday-solutions/flutter_template/tree/41ccdc52b8004bef01469a31f0086cf5ce19c417/lib/flavors
+  ///const String appFlavor = String.fromEnvironment('APP_FLAVOR', defaultValue: 'dev');
+  ///setFlavor(appFlavor);
+  await AppConfig.init();
+  ///await dotenv.load(fileName: ".env");
+  ///runApp(MovieApp());
+  ///runApp(const MyApp());
+
+runApp(EasyLocalization(
+    ///supportedLocales: [ENGLISH_LOCAL, ARABIC_LOCAL],
+    supportedLocales: const [
+        Locale('en'),
+        Locale('fr'),
+      ],
+      fallbackLocale: const Locale('en'),
+      saveLocale: true,
+      useOnlyLangCode: true,
+    ///path: ASSETS_PATH_LOCALISATIONS,
+    path: 'assets/translations',
+    child: Phoenix(child: const MyApp()),
+  ));
+
+  SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     // systemNavigationBarColor: COLORS.grey,
     // statusBarColor: COLORS.grey,
